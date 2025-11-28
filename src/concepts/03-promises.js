781a4526f70f43502f6dbeca3c74a193cdd81b41
Ricardo Martin Metzger Mejia <ricardo7.dev@gmail.com>
2025-11-27 23:49:34 -0500

    feat: problema de promise hell

--- src/concepts/03-promises.js
+++ src/concepts/03-promises.js
@@ -11,8 +11,22 @@ export const promiseComponent = (element) => {
     const renderError = (error) => {
         element.innerHTML = `<h3>${error}</h3>`;
     }
-    const heroID = '5d86371f97c29d020f1e1f6d';
-    findHero(heroID).then(renderHero).catch(renderError);
+    const renderTwoHeroes = (hero1, hero2) => {
+        element.innerHTML = `
+        <h1>${hero1.name}</h1>
+        <h1>${hero2.name}</h1>`;
+    }
+    const heroID1 = '5d86371f97c29d020f1e1f6d';
+    const heroID2 = '5d86371fd55e2e2a30fe1cc4';
+    findHero(heroID1).then(renderHero).catch(renderError);
+
+    let hero1, hero2;
+    findHero(heroID1).then((id1) => {
+        hero1 = id1;
+        findHero(heroID2).then((id2) => {
+            renderTwoHeroes(id1, id2)
+        }).catch(renderError)
+    }).catch(renderError);
 }
 
 /**
