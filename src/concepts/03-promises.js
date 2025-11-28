2fee47045b38583f8b222612983f5699b68bedc2
Ricardo Martin Metzger Mejia <ricardo7.dev@gmail.com>
2025-11-27 23:57:56 -0500

    feat: promise all en JS

--- src/concepts/03-promises.js
+++ src/concepts/03-promises.js
@@ -18,15 +18,40 @@ export const promiseComponent = (element) => {
     }
     const heroID1 = '5d86371f97c29d020f1e1f6d';
     const heroID2 = '5d86371fd55e2e2a30fe1cc4';
-    findHero(heroID1).then(renderHero).catch(renderError);
 
-    let hero1, hero2;
+    //! Version 0 - Promesa tradicional y unica
+    /*
+    findHero(heroID1).then(renderHero).catch(renderError);
+    */
+
+    //! Version 1 - Promise Hell
+    /*
+    let hero1;
     findHero(heroID1).then((id1) => {
         hero1 = id1;
         findHero(heroID2).then((id2) => {
             renderTwoHeroes(id1, id2)
         }).catch(renderError)
     }).catch(renderError);
+    */
+
+    //! Version 2 - Promesas anidadas o en cadena
+    /*
+    let hero1;
+    findHero(heroID1).then(hero => {
+        hero1 = hero;
+        return findHero(heroID2);
+    }).then(x => {
+        renderTwoHeroes(hero1, x);
+    }).catch(renderError);
+    */
+
+    //! Version 3 - Promise All
+    Promise.all([findHero(heroID1), findHero(heroID2)])
+            .then(([hero1, hero2]) => {
+                renderTwoHeroes(hero1, hero2);
+            })
+            .catch(renderError);
 }
 
 /**
